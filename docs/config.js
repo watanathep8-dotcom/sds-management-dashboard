bcca2f4ddce201d2c04fc0734d46e752af14f47d
watanathep8-dotcom <watanathep8@gmail.com>
2026-09-17 13:25:43 +0700

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,7 +1,6 @@
-// Fill this in after deploying the Apps Script web app.
-// Leave apiUrl empty to use browser-local storage until a real API is connected.
+// Central SDS data source on the new Google Apps Script account.
 window.SDS_CONFIG = {
-  apiUrl: "",
-  adminUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzpBSt4H4E0o16Dr2A4uH-copcCJ_wg12svGiWolZa2LBRV92_KOR38Z7sS5XDlhns/exec",
+  adminUrl: "https://script.google.com/u/1/home/projects/1m1t6GPLTIFly8iVZOG9z0MS-tOo85S73xGf8-tEZ-UWeCY3-u8G9i7I9/edit",
   publicReadOnly: true
 };
